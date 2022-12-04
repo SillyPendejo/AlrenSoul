26a27554754bdac07555ac1b6d8be123085242c5
Sunny <blinded@list.ru>
2022-12-04 03:01:46 +0300

    hash router test

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react'
-import { BrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import { default as MainRouter } from './pages'
 import { default as Navigation } from './components/Navigation'
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <div className={'flex flex-1 w-100vw bg-black'}>
         <Navigation />
         <main className={'w-80wv flex flex-1  bg-black'}>
           <MainRouter />
         </main>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
